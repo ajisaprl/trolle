e95bef062f6250a8744c7682ecb446bba5c202f7
Aziz Apriyanto <aziz.apriyanto@bukalapak.com>
2020-05-02 13:43:01 +0700

    edit task functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,9 +9,22 @@ class TasksController < ApplicationController
 		if @task.save
 			flash[:notice] = "Task created!"
 			redirect_to task_path(@task)
-
 		else
 			render 'new'
+		end
+	end
+
+	def edit
+		@task = Task.find(params[:id])
+	end
+
+	def update
+		@task = Task.find(params[:id])
+		if @task.update(tasks_params)
+			flash[:notice] = 'Task was successfully updated'
+			redirect_to task_path(@task)
+		else
+			render 'edit'
 		end
 	end
 
