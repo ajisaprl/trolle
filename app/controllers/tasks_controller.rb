7229fc6d8bb05fbee2b3b9535860d6525dc9e30b
Aziz Apriyanto <aziz.apriyanto@bukalapak.com>
2020-05-02 22:44:43 +0700

    delete functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,9 @@
 class TasksController < ApplicationController
+	before_action :set_task, only: [:edit, :update, :show, :destroy]
+
+	def index
+		@tasks = Task.all
+	end
 
 	def new
 		@task = Task.new
@@ -15,11 +20,11 @@ class TasksController < ApplicationController
 	end
 
 	def edit
-		@task = Task.find(params[:id])
+		
 	end
 
 	def update
-		@task = Task.find(params[:id])
+		
 		if @task.update(tasks_params)
 			flash[:notice] = 'Task was successfully updated'
 			redirect_to task_path(@task)
@@ -29,10 +34,21 @@ class TasksController < ApplicationController
 	end
 
 	def show
-		@task = Task.find(params[:id])
+		
+	end
+
+	def destroy
+		
+		@task.destroy
+		flash[:notice] = 'Task was successfully deleted'
+		redirect_to tasks_path
 	end
 
 	private
+
+	def set_task
+		@task = Task.find(params[:id])
+	end
 
 	def tasks_params
 		params.require(:task).permit(:content)
